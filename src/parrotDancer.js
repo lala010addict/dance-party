dbbccf50f9c434db8a970db440911175542ed911
Yongzhi Huang <yhuang@fastcompany.com>
2016-10-13 22:04:56 -0400

    Give parrot a makeover so he can show off his moves

--- src/parrotDancer.js
+++ src/parrotDancer.js
@@ -1,7 +1,15 @@
 var MakeParrotDancer = function(top, left, timeBetweenSteps){
   MakeDancer.call(this, top, left, timeBetweenSteps);
   this.$node.addClass('parrotDancer');
-  this.$node.css('background-image', "url(https://media2.giphy.com/media/nRBJjjILMTkQM/200.gif)");
+  this.$node.css('background-image', "url(http://i.imgur.com/pbUKAIy.png)");
+
+  var $el = $(this.$node);
+  setInterval(function() {
+	  $el.animate({"left":"+=5px"}, "fast");
+	  $el.animate({"left":"-=6px"}, "fast");
+	  $el.animate({"top":"-=10px"}, "fast");
+		$el.animate({"top":"+=10px"}, "fast");
+  }, 50);
 };
 
 MakeParrotDancer.prototype = Object.create(MakeDancer.prototype);
